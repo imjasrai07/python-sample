2f5f0e8c0b73567f6ac78bb8d3e83a2b697918e0
rj singh <85516771+imjasrai07@users.noreply.github.com>
2022-05-05 08:52:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,83 @@
 import os
-from flask import Flask
 
-app = Flask(__name__)
+import pymongo
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+from wordpress_xmlrpc import Client, WordPressPost
 
-if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+from wordpress_xmlrpc.methods.posts import GetPosts, NewPost
+
+from wordpress_xmlrpc.methods.users import GetUserInfo
+
+dblink=os.environ['dblk']
+
+username=os.environ['unlk']
+
+password= os.environ['pwlk']
+
+weblink=os.environ['ulk']
+
+wp = Client(weblink, username, password)
+
+myclient=pymongo.MongoClient(dblink)
+
+mydb = myclient["articlebase"]
+
+mycol = mydb["content"]
+
+def status(id):
+
+   mycol.update_one({'_id':id},{'$set' : {'status':0}})
+
+def postr(x,n):
+
+  id=x['_id']
+
+  tit=x['title']
+
+  rt=x['slug']
+
+  which = 'publish'
+
+  post = WordPressPost()
+
+  post.post_status = which
+
+  post.title= tit
+
+  post.content=x['content']
+
+  category=x['category']
+
+  post.terms_names ==   { 
+
+  'category' [category]}
+
+  post.slug=rt
+
+  
+
+  wp.call(NewPost(post))
+
+  status(id)
+
+  print(n)
+
+  print('-'*40)
+
+  
+
+y = mycol.find()
+
+i=1
+
+for x in y:
+
+  #postr(x,i)
+
+  id=x['_id']
+
+  status(id)
+
+  print(i)
+
+  i+=1
