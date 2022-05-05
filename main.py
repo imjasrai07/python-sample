b3ed59b43ac61974cb243c7787b8e1d0aad4410e
rj singh <85516771+imjasrai07@users.noreply.github.com>
2022-05-05 10:20:12 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,79 @@
+import os
+
+import pymongo
+
+from wordpress_xmlrpc import Client, WordPressPost
+
+from wordpress_xmlrpc.methods.posts import GetPosts, NewPost
+
+from wordpress_xmlrpc.methods.users import GetUserInfo
+from wordpress_xmlrpc.methods import taxonomies
+
+dblink=os.environ['DBLK']
+
+username=os.environ['UNLK']
+
+password= os.environ['PWLK']
+
+weblink=os.environ['ULK']
+
+wp = Client(weblink, username, password)
+
+myclient=pymongo.MongoClient(dblink)
+
+mydb = myclient["articlebase"]
+
+mycol = mydb["content"]
+
+def status(id):
+
+   mycol.update_one({'_id':id},{'$set' : {'status':1}})
+
+def postr(x,n):
+
+  id=x['_id']
+
+  tit=x['title']
+
+  rt=x['slug']
+
+  which = 'publish'
+
+  post = WordPressPost()
+
+  post.post_status = which
+
+  post.title= tit
+
+  post.content=x['content']
+
+
+  post.slug=rt
+
+  
+
+  wp.call(NewPost(post))
+
+  
+
+  print(n)
+
+  print('-'*40)
+
+  
+
+y = mycol.find()
+
+i=1
+
+for x in y:
+
+  postr(x,i)
+
+  id=x['_id']
+
+  status(id)
+
+  print(i)
+
+  i+=1
